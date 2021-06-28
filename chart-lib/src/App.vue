941ed98f9274c813272fe9d8606d8d5f93764745
LeeGJ <0l055225757@gmail.com>
2021-06-28 17:13:17 +0900

    #210628_3 외부 라이브러리 모듈화 방법(차트) - [실습] LineChart 제작 및 차트 충돌 문제 해결

---
+++ chart-lib/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Chart.js</h1>
+    <bar-chart></bar-chart>
+    <LineChart></LineChart>
+  </div>
+</template>
+
+<script>
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+
+export default {
+  components: {
+    BarChart,
+    LineChart
+  }
+}
+</script>
+
+<style>
+</style>
